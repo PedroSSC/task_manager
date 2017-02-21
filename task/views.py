4b570276eebbcb97ca1d57ef95c8a9cf52d60d30
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-02-20 21:15:30 -0300

    teste2

--- task/views.py
+++ task/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+#Importações para uso de servidor.
 from django.conf.urls import url
 from django.contrib import admin
 from django.http import HttpResponse
 
+#Classe 'Tarefa' criada.
+class Tarefa(object):
+    def __init__(self, titulo, data):
+        self.titulo = titulo
+        self.data = data
+    def __str__(self):
+        return self.titulo
+
+#Funções de respostas de URL.
 def home(request):
     return HttpResponse("Olá!")
 def sobre(request):
